0b9e25474a22faf39620996fcbd338a90ae660d4
Aun Siwat <126206951+siwatis@users.noreply.github.com>
2023-12-05 02:44:18 +0700

    Update script.py
    - Add try except
    - Refactor code order

--- Orderbook_script/script.py
+++ Orderbook_script/script.py
@@ -4,14 +4,14 @@ import schedule
 import time
 import numpy as np
 from datetime import datetime
-        
+    
 pair = input("Give a pair (Ex : DOGEUSDT):")
 print(pair)
 
 spot_api = f'https://www.binance.com/api/v3/depth?symbol={pair}&limit=10'
 futures_api = f'https://www.binance.com/fapi/v1/depth?symbol={pair}&limit=10'
 
-csv_name = f'{pair}.csv'
+csv_name = f'{pair}_{int(time.time())}.csv'
 columns = ['timestamp','market',
            'best_bid_price','best_bid_quant','large_bid_volume',
            'best_ask_price','best_ask_quant','large_ask_volume']
@@ -21,60 +21,68 @@ def generate_header():
         writer = csv.writer(file)
         writer.writerow(columns)
 
-def fetch_spot_data():
-    response = requests.get(spot_api)
-    if response.status_code == 200:
-        data = response.json()
-        timestamp = int(time.time())
-        market = 's'
-        best_bid_price = float(data.get('bids')[0][0])
-        best_bid_quant = float(data.get('bids')[0][1])
-        best_ask_price = float(data.get('asks')[0][0])
-        best_ask_quant = float(data.get('asks')[0][1])
-        large_bid_volume = np.array(data.get("bids")).astype(float).prod(axis=1).sum()
-        large_ask_volume = np.array(data.get("asks")).astype(float).prod(axis=1).sum()
-        data_dict = {columns[0] : timestamp,
-                     columns[1] : market,
-                     columns[2] : best_bid_price,
-                     columns[3] : best_bid_quant,
-                     columns[4] : large_bid_volume,
-                     columns[5] : best_ask_price,
-                     columns[6] : best_ask_quant,
-                     columns[7] : large_ask_volume}
-        return data_dict
-    else:
-        raise Exception(f"API Error: {response.status_code}")
-
-def fetch_futures_data():
-    response = requests.get(futures_api)
-    if response.status_code == 200:
-        data = response.json()
-        market = 'f'
-        timestamp = int(time.time())
-        best_bid_price = float(data.get('bids')[0][0])
-        best_bid_quant = float(data.get('bids')[0][1])
-        best_ask_price = float(data.get('asks')[0][0])
-        best_ask_quant = float(data.get('asks')[0][1])
-        large_bid_volume = np.array(data.get("bids")).astype(float).prod(axis=1).sum()
-        large_ask_volume = np.array(data.get("asks")).astype(float).prod(axis=1).sum()
-        data_dict = {columns[0] : timestamp,
-                     columns[1] : market,
-                     columns[2] : best_bid_price,
-                     columns[3] : best_bid_quant,
-                     columns[4] : large_bid_volume,
-                     columns[5] : best_ask_price,
-                     columns[6] : best_ask_quant,
-                     columns[7] : large_ask_volume}
-        return data_dict
-    else:
-        raise Exception(f"API Error: {response.status_code}")
+def fetch_data():
+    try:
+        spot_response = requests.get(spot_api)
+        futures_response = requests.get(futures_api)
+        timestamp = round(time.time(), 2)
+        spot_data = spot_response.json()
+        best_bid_price = float(spot_data.get('bids')[0][0])
+        best_bid_quant = float(spot_data.get('bids')[0][1])
+        best_ask_price = float(spot_data.get('asks')[0][0])
+        best_ask_quant = float(spot_data.get('asks')[0][1])
+        large_bid_volume = np.array(spot_data.get("bids")).astype(float).prod(axis=1).sum()
+        large_ask_volume = np.array(spot_data.get("asks")).astype(float).prod(axis=1).sum()
+        spot_data_dict = {columns[0] : round(timestamp, 0),
+                    columns[1] : 's',
+                    columns[2] : best_bid_price,
+                    columns[3] : best_bid_quant,
+                    columns[4] : large_bid_volume,
+                    columns[5] : best_ask_price,
+                    columns[6] : best_ask_quant,
+                    columns[7] : large_ask_volume}
+        futures_data = futures_response.json()
+        best_bid_price = float(futures_data.get('bids')[0][0])
+        best_bid_quant = float(futures_data.get('bids')[0][1])
+        best_ask_price = float(futures_data.get('asks')[0][0])
+        best_ask_quant = float(futures_data.get('asks')[0][1])
+        large_bid_volume = np.array(futures_data.get("bids")).astype(float).prod(axis=1).sum()
+        large_ask_volume = np.array(futures_data.get("asks")).astype(float).prod(axis=1).sum()
+        futures_data_dict = {columns[0] : round(timestamp, 0),
+                    columns[1] : 'f',
+                    columns[2] : best_bid_price,
+                    columns[3] : best_bid_quant,
+                    columns[4] : large_bid_volume,
+                    columns[5] : best_ask_price,
+                    columns[6] : best_ask_quant,
+                    columns[7] : large_ask_volume}
+    except:
+        spot_data_dict = {columns[0] : timestamp,
+                    columns[1] : 's',
+                    columns[2] : np.nan,
+                    columns[3] : np.nan,
+                    columns[4] : np.nan,
+                    columns[5] : np.nan,
+                    columns[6] : np.nan,
+                    columns[7] : np.nan}
+        futures_data_dict = {columns[0] : timestamp,
+                    columns[1] : 'f',
+                    columns[2] : np.nan,
+                    columns[3] : np.nan,
+                    columns[4] : np.nan,
+                    columns[5] : np.nan,
+                    columns[6] : np.nan,
+                    columns[7] : np.nan}
+    return [timestamp, spot_data_dict, futures_data_dict]
 
 def append_data_to_csv():
-    spot_data = fetch_spot_data()
-    futures_data = fetch_futures_data()
+    data = fetch_data()
+    spot_data = data[1]
+    futures_data = data[2]
     with open(csv_name, 'a+', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=columns)
         writer.writerow(spot_data)
+        print(datetime.fromtimestamp(data[0]))
         print(spot_data)
         writer.writerow(futures_data)
         print(futures_data)
@@ -83,8 +91,16 @@ def scheduled_data_update():
     append_data_to_csv()
 
 generate_header()
-schedule.every(10).seconds.do(scheduled_data_update)
+
+# Wait to start at 10 seconds
+print('Waiting..')
+while time.time() % 10 >= 0.001:
+    if time.time() % 10 < 0.001:
+        break
+print('Start fetching',pair)
+
+schedule.every(4.56).seconds.do(scheduled_data_update)
 
 while True:
     schedule.run_pending()
-    time.sleep(1)
+    #time.sleep(1)
