747192a76d0fe2dda1dcb7f3263c64cbdf6b24f5
Aun Siwat <126206951+siwatis@users.noreply.github.com>
2024-01-26 19:41:32 +0700

    Update crypto_spot_futures_spread_arbitrage.py
    Add plotting functions

--- crypto_spot_futures_spread_arbitrage.py
+++ crypto_spot_futures_spread_arbitrage.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
+from toolz import pipe
 
 # Set parameters
 pair = 'AVAX-USDT'
@@ -32,7 +33,7 @@ def get_cc_price(pair=pair, days=90, limit=2000, to_timestamp=to_timestamp):
         df.append(concat)
         time.sleep(0.1)
     df = pd.concat(df, axis=0).sort_index()
-    
+
     return df
 
 def backtest(df, rolling=roll, exit_std=exit_std, conservative=False):
@@ -81,3 +82,54 @@ def backtest(df, rolling=roll, exit_std=exit_std, conservative=False):
         df['portfolio_conservative'] = df['expected_return_conservative'].add(1).cumprod().sub(1)
 
     return df
+
+def plot_strategy(df, pair):
+    plt.subplots(figsize=[12,6])
+    plt.fill_between(df.index, df.on_position * df.percent_diff.max(), 
+                     color="cornflowerblue", alpha=0.2)
+    plt.fill_between(df.index, df.on_position * df.percent_diff.min(), 
+                     color="cornflowerblue", alpha=0.2, label='Strategy on position')
+    plt.plot(df.percent_diff, color='mediumslateblue', alpha=0.5, linewidth=0.9, label='Percentage of spread between spot and futures prices')
+    plt.plot(df.percent_diff.rolling(roll).mean(), alpha=0.6, color='darkslateblue', label = f'MA ({roll} minutes moving average of percent spread)')
+    plt.plot(df.percent_diff.rolling(roll).mean() + expected_cost, alpha=0.6, color='mediumaquamarine', label='Entry zone (>= MA + expected transaction costs)')
+    plt.plot(df.percent_diff.rolling(roll).mean() - (2.75*df['percent_diff'].rolling(roll).std()), alpha=0.4, color='firebrick', label=f'Exit zone (<= MA - {exit_std} standard deviation)')
+    #plt.plot(df.percent_diff.rolling(roll).mean() - expected_cost, alpha=0.6, color='firebrick', label='Exit zone')
+    plt.scatter(x = df[df['percent_diff']>df.percent_diff.rolling(roll).mean() + expected_cost].index,
+                y = df[df['percent_diff']>df.percent_diff.rolling(roll).mean() + expected_cost].percent_diff,
+                color='limegreen', s=20, alpha=0.6, label='Entry signal (Open long-spot and short-futures)')
+    plt.scatter(x = df[df['percent_diff']<=df.percent_diff.rolling(roll).mean() - (exit_std * df['percent_diff'].rolling(roll).std())].index,
+                y = df[df['percent_diff']<=df.percent_diff.rolling(roll).mean() - (exit_std * df['percent_diff'].rolling(roll).std())].percent_diff, 
+                color='indianred', s=10, alpha=0.6, label='Exit signal (Close spot and futures positions)')
+    plt.axhline(0, alpha=0.5, color='grey')
+    plt.ylabel('Percent Spread')
+    plt.title(f"Long spot - short futures arbitrage strategy signals of {pair} in 90 days)")
+    #plt.ylim((-0.008,0.003))
+    plt.legend(loc='lower left', ncol = 2)
+    plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1))
+    plt.tight_layout()
+    plt.show()
+
+def plot_portfolio(df, pair, conservative=False):
+    plt.subplots(figsize=[12,3])
+    plt.axhline(0, alpha=0.5, color='grey')
+    plt.plot(df.portfolio, color='mediumslateblue', alpha=0.7, linewidth=1.2, label=f'Cumulative returns: {round(df.portfolio[-1]*100,2)}% at the end')
+    if conservative:
+        try:
+            plt.plot(df.portfolio_conservative, color='sandybrown', alpha=0.7, linewidth=1.2, label=f'Cumulative returns (conservative): {round(df.portfolio_conservative[-1]*100,2)}% at the end')
+        except Exception:
+            pass
+    plt.title(f"Long spot - short futures arbitrage strategy portfolio return of {pair} in 90 days")
+    plt.ylabel('Cumulative portfolio return')
+    try:
+        if (df.portfolio.min() > -0.01) & (df.portfolio_conservative.min() > -0.01):
+            plt.ylim((-0.01))
+    except Exception:
+        pass
+    plt.legend()
+    plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1))
+    plt.tight_layout()
+    plt.show()
+
+def plot_all(df, pair, conservative=False):
+    plot_strategy(df, pair)
+    plot_portfolio(df, pair, conservative=conservative)
