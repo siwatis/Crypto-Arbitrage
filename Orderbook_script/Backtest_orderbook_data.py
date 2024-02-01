9853a35171e3e6ec92de8380edd22b6736d90bbc
Siwat Srisuddee <126206951+siwatis@users.noreply.github.com>
2024-02-01 19:57:32 +0700

    Add backtest function

--- Orderbook_script/Backtest_orderbook_data.py
+++ Orderbook_script/Backtest_orderbook_data.py
@@ -32,5 +32,27 @@ def strategy_signal(data, rolling=(1*24*60*60), expected_cost=0.0015):
     data['signal_group'] = data['signal_entry'].apply(lambda x: 1 if x == True else 0).cumsum().mul(data['signal_entry_zone']).replace(0, np.nan)
     return data
 
+def backtest(data):
+    data.loc[data['signal_entry'].shift(1) == True, 'action'] = 1 # action entry
+    data.loc[data['signal_exit'].shift(1) == True, 'action'] = -1 # action exit
+    data['action'].fillna(0, inplace=True)
+    data['action_cost'] = data['action'].apply(lambda x: 1 if x != 0 else 0)
+    data.loc[(data['action'] == 1) & (data['action'].shift(1) == 0), 'on_position'] = 1
+    data.loc[(data['action'].shift(1) == -1) & (data['action'].shift(2) == 0), 'on_position'] = 0
+    data['on_position'].ffill(inplace=True)
+
+    data['spot_action_entry_price'] = data['spot_bid'].div(data['spot_ask']).sub(1).mul(data['action'].apply(lambda x: 1 if x == 1 else 0))
+    data['spot_rest_price'] = data['spot_bid'].div(data['spot_bid'].shift(1)).sub(1).mul(data['action'].apply(lambda x: 1 if x != 1 else 0))
+    data['spot_return'] = data['spot_action_entry_price'].add(data['spot_rest_price']).mul(data['on_position']).sub(data['action_cost']*0.0015)
+
+    data['futures_action_entry_price'] = data['futures_ask'].div(data['spot_bid']).sub(1).mul(data['action'].apply(lambda x: 1 if x == 1 else 0))
+    data['futures_rest_price'] = data['futures_ask'].div(data['futures_ask'].shift(1)).sub(1).mul(data['action'].apply(lambda x: 1 if x != 1 else 0))
+    data['futures_return'] = data['futures_action_entry_price'].add(data['futures_rest_price']).mul(data['on_position']).sub(data['action_cost']*0.00075)
+
+    data['spot_cumulative_return'] = data['spot_return'].add(1).cumprod().sub(1)
+    data['futures_cumulative_return'] = data['futures_return'].add(1).cumprod().sub(1)
+    data['portfolio_cumulative_return'] = data['spot_return'].add(data['futures']).div(2).add(1).cumprod().sub(1)
+    return data
+
 if __name__ == '__main__':
     pipe('[filename.csv]', read_orderbook_data, prepare_orderbook_data, strategy_signal)
